01021a6b78c2fa3283d455fcb815a22487cb2c8d
Alexander-Diehl <atd5397@e5-cse-goliath.cse.psu.edu>
2024-09-11 00:34:16 -0400

    Your message

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "Alexander-Diehl;
+    char STUDENT_ID[] = "9 4390 1151";
 
     return 0 ;
 }
